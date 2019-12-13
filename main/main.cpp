de27f234d807cb3973d57206ee5bb9c1683c8d2e
ΛŔȾὟΘṂԌ ΛŔȾὟΘṂԌ <nwaga@bk.ru>
2019-12-13 05:04:31 +0300

    Added some examples

--- main/main.cpp
+++ main/main.cpp
@@ -9,6 +9,7 @@ int main()
 {
 	//-11 *  cos(-34)*sin(-123+6 /-3*(-2))+   111*sqrt(100)
 	//-11 *  cos(-34)*sin(-123+6 /-3*(-2))+   111;sqrt(100);50+34*(19+11)
+	//a = -11 *  cos(-34)*sin(-123+6 /-3*(-2))+   111;kor = sqrt(100);50+34*(19+11);c=5*kor;
 	cout << "What do you want to calculate?" << endl;
 	string calculation;
 	getline(cin, calculation);
@@ -18,8 +19,8 @@ int main()
 	for (auto i = results.begin(); i < results.end(); i++)
 		cout << *i << endl;
 
-
-	/*queue<pair<string, ElemType> > parsed = calc.Parse("-11 *  cos(-34)*sin(-123+6 /-3*(-2))+   111+;sqrt(100);50+34*(19+11)");
+	//a = -11 *  cos(-34)*sin(-123+6 /-3*(-2))+   111;sqrt(100);50+34*(19+11);c=5;
+	/*queue<pair<string, ElemType> > parsed = calc.Parse("a = -11 *  cos(-34)*sin(-123+6 /-3*(-2))+   111;kor = sqrt(100);50+34*(19+11);c=5*kor;");
 	//
 	queue<pair<string, ElemType> > parsed2 = parsed;
 	while (!parsed2.empty())
